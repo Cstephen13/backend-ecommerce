b2ec3c7d7d7bde1cbc47edb4ca1c471d084914c6
carlos stiphen Almario Galindez <stiphen.carlos@gmail.com>
2022-04-18 21:30:34 -0500

    toml added

--- backend_e_commerce/config/settings/base.py
+++ backend_e_commerce/config/settings/base.py
@@ -132,6 +132,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '..', "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_collected')
+os.makedirs(STATIC_ROOT, exist_ok=True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

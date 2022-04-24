90fcd9a46786051e392dc4fbff44ea33a36bf674
carlos stiphen Almario Galindez <stiphen.carlos@gmail.com>
2022-04-24 15:13:30 -0500

    s3 storages

--- backend_e_commerce/config/base.py
+++ backend_e_commerce/config/base.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "corsheaders",
     'rest_framework',
+    'storages',
     'products',
     'categories',
     'offers',
     'invoices',
     'api',
-    'core'
+    'core',
+
 ]
 
 MIDDLEWARE = [
@@ -142,20 +144,38 @@ USE_TZ = True
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+USE_S3 = os.environ.get('USE_S3') == 'TRUE'
 
-STATIC_URL = '/static/'
+if USE_S3:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY_ID", '')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_ACCESS_KEY_ID", '')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'core.storages.MediaStorage'
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_collected')
+    os.makedirs(STATIC_ROOT, exist_ok=True)
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    # Media files
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '..', "static"),
+    os.path.join(BASE_DIR,  "static"),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static_collected')
-os.makedirs(STATIC_ROOT, exist_ok=True)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
